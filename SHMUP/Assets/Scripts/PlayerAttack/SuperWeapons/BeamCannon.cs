9f51046cb1dbe6edcd7d1efb513f21544ae4a066
Waldo Rodriguez <waldo.rs@live.com>
2018-03-10 21:04:17 -0500

    More Tweaks
    I messed up the beam cannon and set it to active by default which made it not look right.
    
    Also enemy missiles were not reaching the player cause they fired too early so I bumped up their fuse time to 5 seconds.
    
    PAC has been nerfed in the damage department for being OP. Should still be plenty competitive though.

--- SHMUP/Assets/Scripts/PlayerAttack/SuperWeapons/BeamCannon.cs
+++ SHMUP/Assets/Scripts/PlayerAttack/SuperWeapons/BeamCannon.cs
@@ -39,6 +39,9 @@ public class BeamCannon : MonoBehaviour
         dealDamage = false;
         canIFadeYet = false;
         beamCollider = GetComponent<CapsuleCollider>();
+        //This is the really weird way Unity decided to set colliders to active/inactive.
+        //Since the collider is on by default. This line turns off the collider on start.
+        //When this line pops up again it will turn on the collider.
         beamCollider.enabled = !beamCollider.enabled;
         StartCoroutine(BeamFire());
 	}
