bf9acd203afdc15a34e30ca7704ecf39c6ba2470
Waldo Rodriguez <waldo.rs@live.com>
2018-04-14 17:00:50 -0400

    Eye Boss finished. Fog/Rain Imported. New music imported.
    Fog/Rain and Music waiting on implementation.

--- SHMUP/Assets/Scripts/Enemies/BossScripts/Kerberos/KerberosStage2.cs
+++ SHMUP/Assets/Scripts/Enemies/BossScripts/Kerberos/KerberosStage2.cs
@@ -29,23 +29,15 @@ public class KerberosStage2 : MonoBehaviour
 
     //Lets the ship know it can move
     public bool canIMove;
-    //Lets the ship know it can appear and when to stop increasing its alpha value.
-    public bool canIAppearYet;
-    //Lets the ship know if it can fade out and when to stop.
-    public bool canIFadeYet;
     //Because otherwise explosions go off literally every frame. It's hilarious but wrong.
     public bool stopDying;
-    //How fast the alpha channel is changed.
-    public float fadePerSecond;
-    //How long the fade will last.
-    public float fadeDuration;
-    //How long it will take to appear.
-    public float appearDuration;
 
     //Keeps track of all the guns so the boss knows when to die.
     public GameObject[] gunArray;
     //Holds next boss stage
     public GameObject nextStage;
+
+    public float appearDuration;
 
     //Holds explosions to play during an overly dramatic instance switch.
     public GameObject[] explosions;
@@ -60,47 +52,18 @@ public class KerberosStage2 : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-        canIFadeYet = false;
-        canIAppearYet = false;
         canIMove = false;
         //Gathers all of the child renderers
         childRenderers = GetComponentsInChildren<Renderer>();
         desiredPosition = gameObject.transform.position;
         StartCoroutine(BossAppears());
+
+        Invoke("BossMovement", appearDuration);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Makes the ship appear.
-        if (canIAppearYet)
-        {
-            //Goes through all the child objects and fades them in.
-            foreach (Renderer childObjectColor in childRenderers)
-            {
-                //Keeping this around just for notes. This is how you change material colors
-                //childObjectToFade.material.color = Color.red;
-                childObjectColor.material.color = new Color(childObjectColor.material.color.r, childObjectColor.material.color.g, childObjectColor.material.color.b,
-                    childObjectColor.material.color.a + (fadePerSecond * Time.deltaTime));
-            }
-        }
-        //Checks if the beam can fade or if it's done fading.
-        else if (canIFadeYet)
-        {
-            //Goes through all the children and fades them out.
-            foreach (Renderer childObjectColor in childRenderers)
-            {
-                if (childObjectColor == null)
-                {
-                    continue;
-                }
-                //Keeping this around just for notes. This is how you change material colors
-                //childObjectToFade.material.color = Color.red;
-                childObjectColor.material.color = new Color(childObjectColor.material.color.r, childObjectColor.material.color.g, childObjectColor.material.color.b,
-                    childObjectColor.material.color.a - (fadePerSecond * Time.deltaTime));
-            }
-        }
-
         //Checks if every single gun in the array is destroyed before starting the death coroutine.
         if (gunArray[0] == null && gunArray[1] == null && gunArray[2] == null && gunArray[3] == null && gunArray[4] == null && gunArray[5] == null && gunArray[6] == null && gunArray[7] == null && !stopDying)
         {
@@ -122,7 +85,6 @@ public class KerberosStage2 : MonoBehaviour
 
     IEnumerator BossAppears()
     {
-        canIAppearYet = true;
         yield return new WaitForSeconds(appearDuration);
         foreach (Renderer childObjectColor in childRenderers)
         {
@@ -138,11 +100,9 @@ public class KerberosStage2 : MonoBehaviour
             childObjectColor.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
             childObjectColor.material.renderQueue = -1;
         }
-        canIAppearYet = false;
 
         yield return new WaitForSeconds(startDelay);
         StartCoroutine(BossMovement());
-
     }
 
     IEnumerator DeadStage()
@@ -153,29 +113,7 @@ public class KerberosStage2 : MonoBehaviour
         desiredPosition = deathPosition;
         yield return new WaitForSeconds(3);
 
-        canIFadeYet = true;        
-
-        foreach (Renderer childObjectColor in childRenderers)
-        {
-            if(childObjectColor == null)
-            {
-                continue;
-            }
-            //SetFloat Changes the material shader.
-            //("_Mode", float) I haven't figured out why the first part is needed yet but it is Second part sets the mode.
-            // 0 = Opaque, 1 = Cutout, 2 = Fade, 3 = Transparent
-            childObjectColor.material.SetFloat("_Mode", 2);
-            //After setting the mode we have to set all these other variables or it won't work.
-            childObjectColor.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-            childObjectColor.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-            childObjectColor.material.SetInt("_ZWrite", 0);
-            childObjectColor.material.DisableKeyword("_ALPHATEST_ON");
-            childObjectColor.material.EnableKeyword("_ALPHABLEND_ON");
-            childObjectColor.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-            childObjectColor.material.renderQueue = 3000;
-        }
         yield return new WaitForSeconds(appearDuration);
-        canIFadeYet = false;
         Instantiate(nextStage, gameObject.transform.position, Quaternion.Euler(0,180,0));
         Destroy(gameObject);
     }
--- SHMUP/Assets/Scripts/Enemies/BossScripts/Kerberos/KerberosStage3.cs
+++ SHMUP/Assets/Scripts/Enemies/BossScripts/Kerberos/KerberosStage3.cs
@@ -4,14 +4,147 @@ using UnityEngine;
 
 public class KerberosStage3 : MonoBehaviour
 {
+    private Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //Speed at which boss lerps between objects.
+    public float speed;
+
+    //How long the boss takes before the boss starts moving.
+    public float startDelay;
+
+    //Contains all child object renderers for manipulation.
+    public Component[] childRenderers;
+
+    //Lets the ship know it can move
+    public bool canIMove;
+    public float appearDuration;
+
+    public GameObject[] shotSpawns;
+
+    public float xMin;
+    public float xMax;
+
+    public int dodgeDirection;
+    public float dodgeDuration;
+    public float dodgeDelay;
+
+    //How many shots in a burst.
+    public int burstSize;
+    //How fast the gun shoots.
+    public float fireRate;
+    //Next time guns can fire.
+    public float nextFire;
+    //How often the turret is called to fire.
+    public float nextVolley;
+    //Enemy shot.
+    public GameObject torpedoes;
+    public GameObject beamShot;
+    public GameObject childWeapon1;
+    public GameObject childWeapon2;
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine(deploy());
+        //Invoke("Evade", appearDuration);
+        //InvokeRepeating("fireGuns", startDelay, nextVolley);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //Goes Left
+        if (transform.position.x >= xMax && canIMove)
+        {
+            rb.velocity = new Vector3(-speed, 0, 0);
+        }
+
+        //Goes Right
+        else if (transform.position.x <= xMin && canIMove)
+        {
+            rb.velocity = new Vector3(speed, 0, 0);
+        }
+    }
+
+    IEnumerator Evade()
+    {
+        canIMove = true;
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(0.5f, dodgeDelay));
+            dodgeDirection = Random.Range(0, 2);
+
+            //Goes Left
+            if (dodgeDirection == 0)
+            {
+                rb.velocity = new Vector3(-speed, 0, 0);
+            }
+            //Goes Right
+            else if (dodgeDirection == 1)
+            {
+                rb.velocity = new Vector3(speed, 0, 0);
+            }
+
+            yield return new WaitForSeconds(Random.Range(0.5f, dodgeDuration));
+
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
+
+    IEnumerator deploy()
+    {
+        yield return new WaitForSeconds(startDelay);
+        StartCoroutine(Evade());
+        StartCoroutine(fireGuns());
+    }
+
+
+    IEnumerator fireGuns()
+    {
+        while(true)
+        {
+            int beamSpawn1 = Random.Range(0, 2);
+            int beamSpawn2 = Random.Range(2, shotSpawns.Length);
+            int torpSpawn1;
+            int torpSpawn2;
+
+            if (beamSpawn1 == 0)
+            {
+                torpSpawn1 = 1;
+            }
+            else
+            {
+                torpSpawn1 = 0;
+            }
+
+            if (beamSpawn2 == 2)
+            {
+                torpSpawn2 = 3;
+            }
+            else
+            {
+                torpSpawn2 = 2;
+            }
+
+            //Fires the beam weapons.
+            childWeapon1 = Instantiate(beamShot, shotSpawns[beamSpawn1].transform.position, gameObject.transform.rotation) as GameObject;
+            childWeapon1.transform.parent = gameObject.transform;
+            childWeapon2 = Instantiate(beamShot, shotSpawns[beamSpawn2].transform.position, gameObject.transform.rotation) as GameObject;
+            childWeapon2.transform.parent = gameObject.transform;
+
+            for (int i = 0; i < burstSize; i++)
+            {
+
+                Instantiate(torpedoes, shotSpawns[torpSpawn1].transform.position, shotSpawns[torpSpawn1].transform.rotation);
+                Instantiate(torpedoes, shotSpawns[torpSpawn2].transform.position, shotSpawns[torpSpawn2].transform.rotation);
+                yield return new WaitForSeconds(nextFire);
+            }
+
+            yield return new WaitForSeconds(nextVolley);
+        }
+    }
 }
