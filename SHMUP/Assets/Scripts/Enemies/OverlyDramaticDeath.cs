f94d931be6d6c0bd9b71e9a54468fee627aa2c0e
Waldo Rodriguez <waldo.rs@live.com>
2018-04-20 23:50:21 -0400

    Fixed overly dramatic explosion timing.
    Also fixed multiple corpse spawns and drop roles.

--- SHMUP/Assets/Scripts/Enemies/OverlyDramaticDeath.cs
+++ SHMUP/Assets/Scripts/Enemies/OverlyDramaticDeath.cs
@@ -29,7 +29,7 @@ public class OverlyDramaticDeath : MonoBehaviour
     {
         if (isHeavyScrap)
         {
-            //driftSpeedX = Random.Range(-driftSpeedX, driftSpeedX);
+            driftSpeedX = Random.Range(-driftSpeedX, driftSpeedX);
         }
 
         rb = gameObject.GetComponent<Rigidbody>();
